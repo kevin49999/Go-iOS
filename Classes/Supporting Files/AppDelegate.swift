25d155177baa67144d89ddbc5a079ed9817c25bf
longhorn499 <kevinjohnsonutx@gmail.com>
2019-07-24 23:00:53 -0400

    remove Styles.bootstrap, not needed

--- Classes/Supporting Files/AppDelegate.swift
+++ Classes/Supporting Files/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
-        Styles.bootstrap()
         
         // TODO: #if PROD, need two targets
         FirebaseApp.configure()
