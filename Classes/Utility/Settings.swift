0796c901e87f4a5f89162ddd88f93a0e531d2025
Kevin Johnson <kevinjohnsonutx@gmail.com>
2023-11-17 18:48:56 -0500

    initial

---
+++ Classes/Utility/Settings.swift
@@ -0,0 +1,28 @@
+//
+//  Settings.swift
+//  Go
+//
+//  Created by Kevin Johnson on 11/17/23.
+//  Copyright © 2023 Kevin Johnson. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    enum Items: String {
+        case emojiFeedback = "emoji-feedback"
+        // case haptics?
+    }
+
+    static func emojiFeedback(defaults: UserDefaults = .standard) -> Bool {
+        // not using `bool(forKey:)` because it defaults to `false` if missing a value
+        if let hide = defaults.value(forKey: Items.emojiFeedback.rawValue) as? Bool {
+            return hide
+        }
+        return true
+    }
+
+    static func configure(setting: Items, on: Bool, defaults: UserDefaults = .standard) {
+        defaults.set(on, forKey: setting.rawValue)
+    }
+}
