d0f6182a5bbc908f8b27f0c8c8d9bd711431771d
Kevin Johnson <kevinjohnsonutx@gmail.com>
2025-10-14 08:23:58 -0400

    initial

--- Classes/Supporting Files/AppDelegate.swift
+++ Classes/Supporting Files/AppDelegate.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseCrashlytics
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 }
