8356894baa6db1b2075358f0a9318a3e4441572a
Kevin Johnson <kevinjohnsonutx@gmail.com>
2026-01-27 21:03:58 -0500

    admob (#27)

--- Classes/Supporting Files/AppDelegate.swift
+++ Classes/Supporting Files/AppDelegate.swift
@@ -6,11 +6,14 @@
 //  Copyright © 2019 Kevin Johnson. All rights reserved.
 //
 
+import AdSupport
 import UIKit
 
 import FirebaseCore
 import FirebaseAnalytics
 import FirebaseCrashlytics
+import GoogleMobileAds
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +21,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         FirebaseApp.configure()
+        
+        MobileAds.shared.start(completionHandler: nil)
+
         return true
     }
 }
